d0f2c2b59ff67b30679f673d1602eda38ea7d054
hansik728 <gustlr72838@gmail.com>
2025-02-05 06:24:05 +0900

    1

--- scripts/recover_panda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Recovering Panda; Sit tight and wait for reboot..."
-pkill -f openpilot
-cd /data/openpilot/panda/board
-./recover.sh
-echo "Recovering Panda; If no reboot in 5 min try again.."
-sudo reboot
-reboot
